b1c79d8cb8609c220f1dbe817d03744d7cb96628
Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>
2016-02-17 00:01:02 -0600

    Java: Modified a bit the server
    Now it displays the date, upgrade from 1.0 to 1.1 protocol
    
    Signed-off-by: Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>

--- Task2/HTTP_Server.java
+++ Task2/HTTP_Server.java
@@ -6,20 +6,19 @@ import java.net.*;
 public class HTTP_Server{
     public static void main(String[] args) throws Exception {
 	try {
-            // socket creation
-            int port = 1989;
-            ServerSocket serverSocket = new ServerSocket(port);
-            System.err.println("Server launched on port : " + port);
-            // repeatedly wait for connections, and process
-            while (true) {
-                // you are stuck on waiting for a client request
-                Socket clientSocket = serverSocket.accept();
-                System.err.println("New connected client");
-                Connection c = new Connection(clientSocket);            
-            } 
+        // socket creation
+        int port = 1989;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.err.println("Server running in port : " + port);
+        // repeatedly wait for connections, and process
+        while (true) {
+            // you are stuck on waiting for a client request
+            Socket clientSocket = serverSocket.accept();
+            System.err.println("New client, process it");
+            Connection c = new Connection(clientSocket);
         }
-        catch(IOException e) {
-            System.out.println("Listen :"+e.getMessage());
+    } catch(IOException e) {
+        System.out.println(e.getMessage());
 	}
     }
 }
@@ -28,49 +27,41 @@ class Connection extends Thread {
     BufferedReader in;
     BufferedWriter out;
     Socket clientSocket;
-    public Connection (Socket aClientSocket) {
+    public Connection (Socket nClientSocket) {
         try {
-            // opening a conversation flow
-            clientSocket = aClientSocket;
+            // opening flow
+            clientSocket = nClientSocket;
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
             this.start();
         }
         catch(IOException e) {
-            System.out.println("Connection:"+e.getMessage());
+            System.out.println(e.getMessage());
         }
     }
     public void run() {
         try {
-            // each time data is read from the network is the reference
+            // Each time data is read from the network is the reference
             // on the writing flow. the read data is returned to exactly
             // the same client.
 
-            String s;
-            while ((s = in.readLine()) != null) {
-                System.out.println(s);
-                if (s.isEmpty()) {
+            String str;
+            while ((str = in.readLine()) != null) {
+                if (str.isEmpty()) {
                     break;
                 }
+                System.out.println(str);
             }
-
-            out.write("HTTP/1.0 200 OK\r\n");
-            out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
-            out.write("Server: Apache/0.8.4\r\n");
-            out.write("Content-Type: text/html\r\n");
-            out.write("Content-Length: 59\r\n");
-            out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
-            out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
-            out.write("\r\n");
-            out.write("<TITLE>Test</TITLE>");
-            out.write("<P>This is an test page.</P>");
-            out.write("<P> This is a paragraph test </P>");
             
-            // the flow is closed.
-            System.err.println("Connecting with the customer completed");
+            Date today = new Date();
+            String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+            out.write(httpResponse);
+
+            // Flow is closed.
+            System.err.println("Connecting completed");
         }
         catch(IOException e) {
-            System.out.println("IO:"+e.getMessage());
+            System.out.println(e.getMessage());
         }
         finally{
             try {
@@ -79,7 +70,7 @@ class Connection extends Thread {
                 clientSocket.close();
             }
             catch (IOException e){
-                System.out.println("IO:"+e.getMessage());
+                System.out.println(e.getMessage());
             }
         }
     }
